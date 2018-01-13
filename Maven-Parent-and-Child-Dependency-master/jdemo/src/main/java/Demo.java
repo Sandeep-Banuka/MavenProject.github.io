c7c0648e09506f1967437cfc57ba0d3143191da7
Sandeep-Banuka <34920318+Sandeep-Banuka@users.noreply.github.com>
2018-01-13 20:06:31 +0530

    Maven

---
+++ Maven-Parent-and-Child-Dependency-master/jdemo/src/main/java/Demo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	List l=new ArrayList();
+	public List getData()
+	{
+		
+		l.add("arun");
+		l.add("varun");
+		return l;
+	}
+
+}
